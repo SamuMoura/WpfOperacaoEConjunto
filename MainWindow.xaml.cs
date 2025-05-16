824b56cd0ad58f4f83043e2dd8745177bfcf5fbd
Samuel Moura <152410692+SamuMoura@users.noreply.github.com>
2025-05-15 21:19:36 -0300

    Adicionar arquivos de projeto.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using WpfOpEConj.Model;
+
+namespace WpfOpEConj
+{
+    public partial class MainWindow : Window
+    {
+        private Conjunto conjuntoA = new Conjunto();
+        private Conjunto conjuntoB = new Conjunto();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AdicionarA_Click(object sender, RoutedEventArgs e)
+        {
+            if (int.TryParse(txtElementoA.Text, out int numero))
+            {
+                conjuntoA.Adicionar(numero);
+                AtualizarListBox(lstA, conjuntoA);
+                txtElementoA.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Insira um número inteiro válido.");
+            }
+        }
+
+        private void RemoverA_Click(object sender, RoutedEventArgs e)
+        {
+            if (int.TryParse(txtElementoA.Text, out int numero))
+            {
+                conjuntoA.Remover(numero);
+                AtualizarListBox(lstA, conjuntoA);
+                txtElementoA.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Insira um número inteiro válido.");
+            }
+        }
+
+        private void AdicionarB_Click(object sender, RoutedEventArgs e)
+        {
+            if (int.TryParse(txtElementoB.Text, out int numero))
+            {
+                conjuntoB.Adicionar(numero);
+                AtualizarListBox(lstB, conjuntoB);
+                txtElementoB.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Insira um número inteiro válido.");
+            }
+        }
+
+        private void RemoverB_Click(object sender, RoutedEventArgs e)
+        {
+            if (int.TryParse(txtElementoB.Text, out int numero))
+            {
+                conjuntoB.Remover(numero);
+                AtualizarListBox(lstB, conjuntoB);
+                txtElementoB.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Insira um número inteiro válido.");
+            }
+        }
+
+        private void AtualizarListBox(ListBox listBox, Conjunto conjunto)
+        {
+            listBox.ItemsSource = null;
+            listBox.ItemsSource = conjunto.Elementos;
+        }
+    }
+}
---
+++ Model/Conjunto.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace WpfOpEConj.Model
+{
+    public class Conjunto
+    {
+        public List<int> Elementos { get; private set; }
+
+        public Conjunto()
+        {
+            Elementos = new List<int>();
+        }
+
+        public void Adicionar(int elemento)
+        {
+            if (!Elementos.Contains(elemento))
+                Elementos.Add(elemento);
+        }
+
+        public void Remover(int elemento)
+        {
+            Elementos.Remove(elemento);
+        }
+    }
+}
